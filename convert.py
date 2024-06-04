17d310a92b9bc7942eb136282f444c0a2194c7da
Pride <20098992+PrideInt@users.noreply.github.com>
2024-06-04 00:23:32 -0400

    selective control over what files you want to convert

--- convert.py
+++ convert.py
@@ -10,8 +10,22 @@ process = input("Do you want to convert FASTQ files to FASTA files? (y/n): ")
 while True:
     if process == "y" or process == "Y":
         for fastq_file in os.listdir(fastq_folder):
-            print("Converting " + fastq_file + " to FASTA.")
-            SeqIO.convert(fastq_folder + fastq_file, "fastq", fastq_folder + fastq_file.split(".")[0] + ".fasta", "fasta")
+            if fastq_file.endswith(".fasta"):
+                print("Skipping " + fastq_file + " as it is already in FASTA format.")
+                continue
+            else:
+                convert = input("Do you want to convert " + fastq_file + " to FASTA? (y/n): ")
+
+                while True:
+                    if convert == "y" or convert == "Y":
+                        print("Converting " + fastq_file + " to FASTA.")
+                        SeqIO.convert(fastq_folder + fastq_file, "fastq", fastq_folder + fastq_file.split(".")[0] + ".fasta", "fasta")
+                        break
+                    elif convert == "n" or convert == "N":
+                        print("Skipping " + fastq_file + ".")
+                        break
+                    else:
+                        convert = input("Invalid input. Please enter 'y' or 'n': ")
 
         print("Conversion complete.")
         break
@@ -26,11 +40,25 @@ process = input("Do you want to convert GenBank files to FASTA files? (y/n): ")
 while True:
     if process == "y" or process == "Y":
         for genbank_file in os.listdir(genbank_folder):
-            with open(genbank_file, "rU") as input_handle:
-                with open(genbank_file.split(".")[0] + ".fasta", "w") as output_handle:
-                    print("Converting " + genbank_file + " to FASTA.")
-                    sequences = SeqIO.parse(input_handle, "genbank")
-                    count = SeqIO.write(sequences, output_handle, "fasta")
+            if genbank_file.endswith(".fasta"):
+                print("Skipping " + genbank_file + " as it is already in FASTA format.")
+                continue
+            else:
+                convert = input("Do you want to convert " + genbank_file + " to FASTA? (y/n): ")
+
+                while True:
+                    if convert == "y" or convert == "Y":
+                        with open(genbank_file, "rU") as input_handle:
+                            with open(genbank_file.split(".")[0] + ".fasta", "w") as output_handle:
+                                print("Converting " + genbank_file + " to FASTA.")
+                                sequences = SeqIO.parse(input_handle, "genbank")
+                                count = SeqIO.write(sequences, output_handle, "fasta")
+                        break
+                    elif convert == "n" or convert == "N":
+                        print("Skipping " + genbank_file + ".")
+                        break
+                    else:
+                        convert = input("Invalid input. Please enter 'y' or 'n': ")
 
         print("Conversion complete.")
         break
